b8a897d011b03db4d71c89dc6769b4ab9050b8e4
romele-stefano <romele.stefano@gmail.com>
2020-11-28 15:41:01 +0100

    Resolved validation errore

--- validation.js
+++ validation.js
@@ -7,7 +7,7 @@ const registerValidation = (data) => {
     email: Joi.string().min(6).max(255).required().email(),
     password: Joi.string().min(6).max(1024).required()
   })
-  return schema.validate(data, schema)
+  return schema.validate(data)
 }
 
 // login validation
@@ -16,7 +16,7 @@ const loginValidation = (data) => {
     email: Joi.string().min(6).max(255).required().email(),
     password: Joi.string().min(6).max(1024).required()
   })
-  return schema.validate(data, schema)
+  return schema.validate(data)
 }
 
 module.exports.registerValidation = registerValidation
